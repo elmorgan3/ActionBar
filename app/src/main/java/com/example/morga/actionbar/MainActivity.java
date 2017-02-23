eab987014bcd5c416a940421789fb28d842f2339
Morgan <morgan.cordoba@hotmail.com>
2017-02-23 16:16:05 +0100

    Ejemplo de como se usa un Action Bar

---
+++ app/src/main/java/com/example/morga/actionbar/MainActivity.java
@@ -0,0 +1,41 @@
+package com.example.morga.actionbar;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu (Menu miMenu)
+    {
+        getMenuInflater().inflate(R.menu.menu_activity, miMenu);
+
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_nuevo:
+                Log.i("ActionBar", "Nuevo!");
+                return true;
+            case R.id.action_buscar:
+                Log.i("ActionBar", "Buscar!");;
+                return true;
+            case R.id.action_settings:
+                Log.i("ActionBar", "Settings!");;
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+}
